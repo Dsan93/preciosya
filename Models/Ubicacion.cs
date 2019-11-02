cedf674238fc7983fdd85c8f6b6b2b6cbfda563d
Alan <serapio_alan@hotmail.com>
2019-11-01 22:23:22 -0300

    version 1.0
    version principal

---
+++ Models/Ubicacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace preciosya
+{
+    public partial class Ubicacion
+    {
+        public Ubicacion()
+        {
+            Comercio = new HashSet<Comercio>();
+        }
+
+        public int UbId { get; set; }
+        public int? UbParId { get; set; }
+
+        public virtual Partido UbPar { get; set; }
+        public virtual ICollection<Comercio> Comercio { get; set; }
+    }
+}
